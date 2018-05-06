db0b96d393d7efbe11ee927b6302274296e78acb
TankRaj <tankraj4444@gmail.com>
2018-05-06 15:46:14 +0545

    removed the ktlin code of model class to remove migration bug, :DD

---
+++ app/src/main/java/com/tanka/accessories/todoroom/data/model/Note.java
@@ -0,0 +1,72 @@
+package com.tanka.accessories.todoroom.data.model;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by access-tanka on 11/16/17.
+ */
+
+@Entity
+public class Note implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    public int id;
+
+    @ColumnInfo(name = "title")
+    public String title;
+
+    @ColumnInfo(name = "date")
+    public String date;
+
+    @ColumnInfo(name = "body")
+    public String body;
+
+    @ColumnInfo(name = "type")
+    public String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
